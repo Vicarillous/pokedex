f9463124e724bc19956f76c8304b8f7c92d70e3a
Vinícius Marques <marquesv473@gmail.com>
2022-07-14 02:56:00 -0400

    Adicionado nova variante de header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
 		<header className="container p-6 mx-auto flex justify-center">
-			<h1 className="text-center inline-block font-bold text-7xl text-slate-600 transition-all duration-700 hover:text-red-200">
-				<a href="#">
+			<h1 className="text-center inline-block font-bold text-7xl text-slate-600 transition-all duration-700 hover:text-red-200 select-none">
+				<Link to="/">
 					Pokedex
-				</a>
+				</Link>
 			</h1>
 		</header>
   );
+}
+
+export const SmallHeader = () => {
+	return (
+		<header className="container p-6 mx-auto flex justify-center">
+			<h1 className="text-center inline-block font-bold text-7xl text-slate-600 transition-all duration-700 hover:text-red-200 z-10 select-none">
+				<Link to="/">Pokedex</Link>
+			</h1>
+		</header>
+	);
 }
 
 export default Header
