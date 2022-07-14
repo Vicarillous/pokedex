0e06a0a1878c9cd4713f6ccfdf71eb71866c303a
Vinícius Marques <marquesv473@gmail.com>
2022-07-14 02:54:18 -0400

    Adicionado spinner de loading

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,93 @@
+import "styles/Spinner.css";
+
+export const Spinner = ({ className, color }) => {
+	return (
+		<div className={className}>
+			<svg
+				id="eyEQf8vXUpK1"
+				xmlns="http://www.w3.org/2000/svg"
+				xmlnsXlink="http://www.w3.org/1999/xlink"
+				viewBox="-25 -25 200 200"
+				shapeRendering="geometricPrecision"
+				textRendering="geometricPrecision"
+				className={color ? `${color}` : "fill-slate-400"}
+			>
+				<g
+					id="eyEQf8vXUpK2_tr"
+					transform="translate(74.5,74.79) rotate(0)"
+				>
+					<g id="eyEQf8vXUpK2_ts" transform="scale(1,1)">
+						<path
+							d="M84.89,13.74c12.461772,1.882532,24.061048,7.495647,33.27,16.1L127,21C98.106601,-6.332843,52.893399,-6.332843,24,21l8.84,8.84c13.956716-13.042732,33.111062-18.987946,52-16.14"
+							transform="translate(-75.34,-75.33)"
+						/>
+					</g>
+				</g>
+				<g
+					id="eyEQf8vXUpK3_tr"
+					transform="translate(74.5,75) rotate(0)"
+				>
+					<g id="eyEQf8vXUpK3_ts" transform="scale(1,1)">
+						<path
+							d="M32.88,29.88L24,21C11.409524,32.904396,3.340488,48.809197,1.17,66h12.61c2.132697-13.84231,8.860193-26.564673,19.1-36.12Z"
+							transform="translate(-75.34,-75.54)"
+						/>
+					</g>
+				</g>
+				<g
+					id="eyEQf8vXUpK4_tr"
+					transform="translate(74.5,74.79) rotate(0)"
+				>
+					<g id="eyEQf8vXUpK4_ts" transform="scale(1,1)">
+						<path
+							d="M127.92,109.63c-16.826886,25.832016-49.96634,35.551451-78.08,22.9l6.25-10.93c-8.882542-3.767847-16.489888-10.017606-21.91-18l-10.07,7.44-10,7.42c14.228957,20.395622,37.647802,32.405432,62.513933,32.058817s47.941127-13.004514,61.596067-33.788817Z"
+							transform="translate(-75.34,-75.33)"
+						/>
+					</g>
+				</g>
+				<g
+					id="eyEQf8vXUpK5_tr"
+					transform="translate(74.5,74.79) rotate(0)"
+				>
+					<g id="eyEQf8vXUpK5_ts" transform="scale(1,1)">
+						<path
+							d="M137.71,82h-12.6c-2.021019,15.476958-11.153622,29.12226-24.690958,36.891513s-29.926206,8.771059-44.309042,2.708487l-6.25,10.93c28.11366,12.651451,61.253114,2.932016,78.08-22.9l10.26,7.11c6.855044-10.391867,10.997789-22.335348,12.05-34.74Z"
+							transform="translate(-75.34,-75.33)"
+						/>
+					</g>
+				</g>
+				<g id="eyEQf8vXUpK6_tr" transform="translate(75,75) rotate(0)">
+					<g id="eyEQf8vXUpK6_ts" transform="scale(1,1)">
+						<path
+							d="M127,21l-8.84,8.84c-9.208952-8.604353-20.808228-14.217468-33.27-16.1L83.13,26.12c-2.51217-.380812-5.049136-.574678-7.59-.58-4.116536.000501-8.217362.507647-12.21,1.51l4.49,14.36c18.138407-4.100536,36.316706,6.69928,41.39,24.59h40.7C147.718782,48.798353,139.620637,32.891913,127,21Z"
+							transform="translate(-75.84,-75.54)"
+						/>
+					</g>
+				</g>
+				<g
+					id="eyEQf8vXUpK7_tr"
+					transform="translate(74.154821,75) rotate(0)"
+				>
+					<g id="eyEQf8vXUpK7_ts" transform="scale(1,1)">
+						<path
+							d="M75.54,13c-30.841205.015262-57.063774,22.518438-61.76,53h28.09c3.49581-12.329373,13.450466-21.762321,25.95-24.59L63.33,27.05c3.992638-1.002353,8.093464-1.509499,12.21-1.51c2.540864.005322,5.07783.199188,7.59.58l1.76-12.38c-3.093794-.479981-6.21922-.727341-9.35-.74Z"
+							transform="translate(-75.34,-75.54)"
+						/>
+					</g>
+				</g>
+				<g
+					id="eyEQf8vXUpK8_tr"
+					transform="translate(74.5,74.79) rotate(0)"
+				>
+					<g id="eyEQf8vXUpK8_ts" transform="scale(1,1)">
+						<path
+							d="M109.94,82c-3.115948,16.540315-17.563744,28.520496-34.395,28.520496s-31.279052-11.980181-34.395-28.520496h-40.31c1.113208,13.111419,5.677857,25.694394,13.23,36.47l10-7.42l10.07-7.44c11.656501,17.195636,32.731462,25.392222,52.943128,20.590924s35.348944-21.600133,38.026872-42.200924Z"
+							transform="translate(-75.34,-75.33)"
+						/>
+					</g>
+				</g>
+				<circle r="25" transform="translate(74.5 74.79)" />
+			</svg>
+		</div>
+	);
+};
