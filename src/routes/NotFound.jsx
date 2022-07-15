be75faf84a507f78e04ebc17b873b6c5f0f7d90d
Vinícius Marques <marquesv473@gmail.com>
2022-07-15 01:47:28 -0400

    Adicionado rota NotFound

---
+++ src/routes/NotFound.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import ErrorImage from "assets/sad_luxio.png";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+	return (
+		<div className="container max-w-3xl px-6 md:px-0 mx-auto h-screen flex justify-center items-center flex-col text-slate-600 text-center">
+			<img src={ErrorImage} />
+			<h1 className="text-9xl font-bold">#404</h1>
+			<p className="text-6xl font-light leading-normal">
+				Opps! Página não encontrada
+			</p>
+			<p className="text-2xl font-light leading-normal">
+                A página que você estava procurando não existe. Você pode ter digitado o endereço errado ou a página foi removida.
+			</p>
+			<Link
+				to="/"
+				className="rounded-xl bg-red-400 shadow-lg shadow-red-400/50 text-white text-lg font-normal p-3 px-6 m-6"
+			>
+				Voltar para o início
+			</Link>
+		</div>
+	);
+};
+
+export default NotFound;
