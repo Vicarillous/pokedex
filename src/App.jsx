e892a7adbb53f2846bba9b40c84e138299c88c84
Vinícius Marques <marquesv473@gmail.com>
2022-07-15 01:51:21 -0400

    Adicionado rota NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState, useRef } from "react";
 import { useLazyQuery } from "@apollo/client";
+
 import "App.css";
 import pokeball from "assets/poke_ball_icon.png";
 import { GET_POKEMONS_BY_NAME, GET_POKEMONS_BY_ID } from "api/PokedexQueries";
 import PokedexApi from "api/PokedexApi";
 import Pokedex from "components/pokedex/Pokedex";
 import Header from "components/Header";
-import SearchBar from 'components/SearchBar';
+import SearchBar from "components/SearchBar";
 
 function App() {
 	const [pokemons, setPokemons] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
 	const [isSearch, setIsSearch] = useState(false);
 	const [totalPages, setTotalPages] = useState(0);
-	const [currentPage, setCurrentPage] = useState(parseInt(sessionStorage.getItem("currentPage")) || 0);
+	const [currentPage, setCurrentPage] = useState(
+		parseInt(sessionStorage.getItem("currentPage")) || 0
+	);
+	const [notFound, setNotFound] = useState(false);
 	const itemsPerPage = 40;
 
 	const [getPokemonsByName] = useLazyQuery(GET_POKEMONS_BY_NAME);
@@ -44,18 +48,22 @@ function App() {
 			setIsLoading(false);
 			setTotalPages(Math.ceil(data.count / itemsPerPage));
 		} catch (e) {
-			console.log(e)
+			console.log(e);
 		}
 	};
 
 	const searchPokemon = async (search) => {
+		setNotFound(false);
 		setIsSearch(true);
+
 		if (!search) {
 			setIsSearch(false);
 			return fetchPokemons();
 		}
+
 		setIsLoading(true);
 
+		search = search.toLowerCase();
 		let searchData;
 
 		if (isNaN(search)) {
@@ -80,12 +88,16 @@ function App() {
 		setTotalPages(Math.ceil(totalCount.current / itemsPerPage));
 		setCurrentPage(0);
 		sessionStorage.setItem("currentPage", currentPage);
-		
 		setPokemons(results);
 		setIsLoading(false);
+
+		if (results.length === 0) {
+			setNotFound(true);
+		}
 	};
 
 	useEffect(() => {
+		setNotFound(false);
 		fetchPokemons();
 		sessionStorage.setItem("currentPage", currentPage);
 	}, [currentPage, isSearch]);
@@ -100,15 +112,24 @@ function App() {
 			<div className="container p-8 mx-auto">
 				<Header />
 				<SearchBar searchPokemon={searchPokemon} />
-				<Pokedex
-					pokemons={pokemons}
-					isLoading={isLoading}
-					currentPage={currentPage}
-					totalPages={totalPages}
-					itemsPerPage={itemsPerPage}
-					totalCount={totalCount.current}
-					setCurrentPage={setCurrentPage}
-				/>
+				{notFound ? (
+					<div className="border-2 border-red-400 rounded-lg max-w-lg mx-auto my-12 py-3 px-6 bg-white text-center">
+						<p className="text-red-400 text-lg font-semibold">
+							Nenhum Pokémon corresponde à sua pesquisa!
+						</p>
+						<p className="text-lg font-regular text-slate-700">Altere o termo de busca e tente novamente.</p>
+					</div>
+				) : (
+					<Pokedex
+						pokemons={pokemons}
+						isLoading={isLoading}
+						currentPage={currentPage}
+						totalPages={totalPages}
+						itemsPerPage={itemsPerPage}
+						totalCount={totalCount.current}
+						setCurrentPage={setCurrentPage}
+					/>
+				)}
 			</div>
 		</>
 	);
