12f2e15bc89c8dbdfe4035b83a207dad10685c78
Vinícius Marques <marquesv473@gmail.com>
2022-07-15 01:51:07 -0400

    Adicionado procurar ao pressionar enter

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,13 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useKeyPress } from "hooks/customHooks";
 import pokeball from "../assets/poke_ball_icon.png";
 
 
 const SearchBar = ({searchPokemon}) => {
     const [search, setSearch] = useState("");
+	const searchInput = useRef(null);
+	const enter = useKeyPress("Enter");
 
     const handleChange = (event) => {
 		setSearch(event.target.value);
 	};
+
+	useEffect(() => {
+		if (enter && document.activeElement === searchInput.current) {
+			searchPokemon(search);
+		}
+	}, [enter]);
 
 	return (
 		<div className="w-96 md:w-7/12 mx-auto bg-white p-4 rounded-2xl shadow-lg flex">
@@ -16,6 +25,7 @@ const SearchBar = ({searchPokemon}) => {
 				placeholder="Ditto"
 				onChange={handleChange}
 				value={search}
+				ref={searchInput}
 			/>
 			<button
 				className="bg-red-400 rounded-xl w-14 h-14 p-2 shadow-lg shadow-red-400/50 mx-3 shrink-0"
